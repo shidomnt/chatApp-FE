77d2d75cdcb04ea4e42c995ea90e2397f1b93540
dachieu2k2 <hieutedam2k2@gmail.com>
2022-02-09 22:41:09 +0700

    context user

--- src/contexts/UserProvider.js
+++ src/contexts/UserProvider.js
@@ -1,18 +1,62 @@
-import React, {useState, createContext } from "react"
+import React, { useState, createContext, useEffect } from "react";
+import axios from "axios";
+import { apiUrl } from "./constants";
 
-const UserContext = createContext()
+const UserContext = createContext();
 
-function UserProvider({ children }){
+function UserProvider({ children }) {
   const [user, setUser] = useState(null);
 
-  return(
-    <UserContext.Provider value={{user, setUser}}>
+  const getUser = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        throw new Error("loi khong co token");
+      }
+      const response = await axios.get(`${apiUrl}/users`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.data.success) {
+        setUser(response.data.info);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => getUser(), []);
+
+  const login = async (data) => {
+    try {
+      const response = await axios.post(`${apiUrl}/users/login`, data);
+      console.log(response.data);
+      if (response.data.success) {
+        localStorage.setItem("token", response.data.accessToken);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const register = async (data) => {
+    try {
+      const response = await axios.post(`${apiUrl}/users/register`, data);
+      console.log(response.data);
+      if (response.data.success) {
+        localStorage.setItem("token", response.data.accessToken);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const userContextData = { user, login, register };
+
+  return (
+    <UserContext.Provider value={userContextData}>
       {children}
     </UserContext.Provider>
-    )
+  );
 }
 
-export default UserProvider
-export {
-  UserContext
-}
+export default UserProvider;
+export { UserContext };
---
+++ src/contexts/constants.js
@@ -0,0 +1,2 @@
+export const apiUrl =
+  "http://b9cc-2405-4800-74ce-4b0a-b283-feff-fea2-ff23.ngrok.io/api";
