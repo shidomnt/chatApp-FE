d2aa05f38ba4c0975e71f84e8b3c45a800b94c4f
dachieu2k2 <hieutedam2k2@gmail.com>
2022-02-15 21:21:31 +0700

    a

--- src/components/Modals/CreateRoomModal.js
+++ src/components/Modals/CreateRoomModal.js
@@ -1,17 +1,18 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Modal, Input, Button, Space, Typography, Mentions } from 'antd';
-import { EditOutlined } from '@ant-design/icons';
-import axios from 'axios';
+import React, { useContext, useEffect, useState } from "react";
+import { Modal, Input, Button, Space, Typography, Mentions } from "antd";
+import { EditOutlined } from "@ant-design/icons";
+import axios from "axios";
 
-import { AppContext } from '../../contexts/AppProvider';
-import { apiUrl } from '../../contexts/constants';
+import { AppContext } from "../../contexts/AppProvider";
+import { apiUrl } from "../../contexts/constants";
 
 const CreateRoomModal = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [roomName, setRoomName] = useState('');
+  const [roomName, setRoomName] = useState("");
   const [friendList, setFriendList] = useState([]);
-  const [friendName, setFriendName] = useState('');
+  const [friendName, setFriendName] = useState("");
 
+  console.log(friendName);
   const { createRoom } = useContext(AppContext);
 
   useEffect(() => {
@@ -30,14 +31,14 @@ const CreateRoomModal = () => {
     if (roomName && friendName) {
       await createRoom({ name: roomName, username: friendName });
     }
-    setFriendName('');
-    setRoomName('');
+    setFriendName("");
+    setRoomName("");
     setIsModalVisible(false);
   };
 
   const handleCancel = () => {
-    setRoomName('');
-    setFriendName('');
+    setRoomName("");
+    setFriendName("");
     setIsModalVisible(false);
   };
 
@@ -55,14 +56,14 @@ const CreateRoomModal = () => {
         onOk={handleOk}
         onCancel={handleCancel}
       >
-        <Space direction="vertical" size="large" style={{ width: '100%' }}>
+        <Space direction="vertical" size="large" style={{ width: "100%" }}>
           <Input
             placeholder="Enter room's name..."
             value={roomName}
             onChange={(e) => setRoomName(e.target.value)}
           />
           <Mentions
-            style={{ width: '100%' }}
+            style={{ width: "100%" }}
             onSelect={(option) => setFriendName(option.value)}
             defaultValue={friendName}
             placeholder="Input @ to mention your friends"
